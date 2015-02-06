4a7eeb0c2a9a0a7b172af1c78bc34041d853f6b6
Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-02-05 16:56:09 -0800

    Add html for user login and new account forms

--- app.rb
+++ app.rb
@@ -2,3 +2,27 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file}
+
+get '/' do
+  erb(:index)
+end
+
+
+post '/create_account' do
+  login = params.fetch("login_name")
+  if login.include?('@') && login.include?('.')
+    @user = User.create(email: login)
+  else
+    @user = User.create(name: login)
+    erb(:account)
+end
+
+
+patch '/login' do
+  login = params.fetch("login_name")
+  if login.include?('@') && login.include?('.')
+    @user = User.where(email: login)
+  else
+    @user = User.where(name: login)
+  erb(:account)
+end
